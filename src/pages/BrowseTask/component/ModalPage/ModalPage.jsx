eb28f59b24e782fa87fce2fc3b0656c101738034
Ziwei Lan <ziweilan92@gmail.com>
2020-09-07 23:58:52 +1000

    adjust modal style

--- src/pages/BrowseTask/component/ModalPage/ModalPage.jsx
+++ src/pages/BrowseTask/component/ModalPage/ModalPage.jsx
@@ -4,15 +4,17 @@ import styles from './ModalPage.module.scss';
 
 export default function ModalPage({ header, content, footer}) {
   return (
-    <div className={styles.modal_page} >
-      <div className={styles.header} >
-        {header}
-      </div>
-      <div className={styles.content} >
-        {content}
-      </div>
-      <div className={styles.footer} >
-        {footer}
+    <div className={styles.overlay} >
+      <div className={styles.modal_page} >
+        <div className={styles.header} >
+          {header}
+        </div>
+        <div className={styles.content} >
+          {content}
+        </div>
+        <div className={styles.footer} >
+          {footer}
+        </div>
       </div>
     </div>
   );

b7ee2e1d9212001faf9744fc5d6dc7f703301ef6
Ziwei Lan <ziweilan92@gmail.com>
2020-09-09 13:14:37 +1000

    solve conflict

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
@@ -15,12 +15,36 @@ import NotFound from './pages/NotFound';
 import CreateProfile from './pages/BrowseTask/component/CreateProfile';
 import { ModalControlProvider } from './ModalContext';
 
+const customStyles = {
+  content : {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    display: 'flex',
+    borderRadius: '10px',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
+
+Modal.setAppElement('#root');
+
 const App = () => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [modalContent, setModal] = useState('');
+  
+  const toggleModal = () => {
+    setIsOpen(!modalIsOpen);
+  }
+
+  const modalControl = { 
+    toggleModal,
+    setModal, 
+  };
 
   return (
     <AuthProvider>
-<<<<<<< HEAD
-<<<<<<< HEAD
       <Router>
         <Switch>
           <Route exact path="/" component={Landing} />
@@ -31,58 +55,6 @@ const App = () => {
           <Route path="*" component={NotFound} />
         </Switch>
       </Router>
-||||||| parent of ccd2037... 1. introduce 'react-modal' package;
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route path="/tasks" component={BrowseTask} />
-          <Route exact path="/create-profile" component={CreateProfile} />
-          <Route exact path="/create" component={PostTask} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/login" component={Login} />
-          <PrivateRoute exact path="/profile" component={Profile} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </Router>
-=======
-      <ModalControlProvider value={modalControl}>
-||||||| parent of d4e2e68... 1. move all modal related code into "ModalContext";
-      <ModalControlProvider value={modalControl}>
-=======
-      <ModalControlProvider>
->>>>>>> d4e2e68... 1. move all modal related code into "ModalContext";
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route path="/tasks" component={BrowseTask} />
-            <Route exact path="/create-profile" component={CreateProfile} />
-            <Route exact path="/create" component={PostTask} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/login" component={Login} />
-            <PrivateRoute exact path="/profile" component={Profile} />
-            <Route path="*" component={NotFound} />
-          </Switch>
-        </Router>
-      </ModalControlProvider>
-<<<<<<< HEAD
-      <Modal 
-        isOpen={modalIsOpen}
-        onRequestClose={toggleModal}
-        style={customStyles}
-      >
-        {modalContent}
-      </Modal>
->>>>>>> ccd2037... 1. introduce 'react-modal' package;
-||||||| parent of d4e2e68... 1. move all modal related code into "ModalContext";
-      <Modal 
-        isOpen={modalIsOpen}
-        onRequestClose={toggleModal}
-        style={customStyles}
-      >
-        {modalContent}
-      </Modal>
-=======
->>>>>>> d4e2e68... 1. move all modal related code into "ModalContext";
     </AuthProvider>
   );
 }

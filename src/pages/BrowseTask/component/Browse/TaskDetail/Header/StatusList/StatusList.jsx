c986ca196dc335816d2389febe97fd38e867d163
Ziwei Lan <ziweilan92@gmail.com>
2020-09-10 15:47:56 +1000

    import "status" instead of typing in string

--- src/pages/BrowseTask/component/Browse/TaskDetail/Header/StatusList/StatusList.jsx
+++ src/pages/BrowseTask/component/Browse/TaskDetail/Header/StatusList/StatusList.jsx
@@ -3,18 +3,20 @@ import classNames from 'classnames/bind';
 
 import styles from './StatusList.module.scss';
 
+import * as STATUS from '../../../../../../../components/Status'; 
+
 function StatusList({ status }) {
   const cx = classNames.bind(styles);
 
-  const statusNameList = ['open', 'assigned', 'completed', 'expired'];
+  const statusNameList = Object.values(STATUS);
   const statusList = statusNameList.map((statusName) => (
     <div 
       className = {cx(
-        {
+        { 
           status: true,
-          active: status.toUpperCase() === statusName.toUpperCase(),
+          active: status === statusName,
         },
-        statusName
+        statusName.toLowerCase()
       )}
       key={statusName}
     >

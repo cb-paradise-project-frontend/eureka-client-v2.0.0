c9978a828e795f912bdee7d1f8123a3174c6ce6f
Changhong Lin <changhong.linst@gmail.com>
2020-09-10 16:20:30 +1000

    make FormTextAreaInput as public component

--- src/pages/PostTask/PostTaskForm/TaskDescription/JobDetailsInput/JobDetailsInput.jsx
+++ src/pages/PostTask/PostTaskForm/TaskDescription/JobDetailsInput/JobDetailsInput.jsx
@@ -1,28 +1,37 @@
 import React from 'react'
 
-import TaskInput from '../../../TaskInput';
+import styles from '../../PostTaskForm.module.scss'
+
+import FormTextAreaInput from '../../../../../components/FormTextAreaInput';
 
 export default function JobDetailsInput({
   jobDetails,
   isJobDetailsValid,
   onJobDetails,
+  errorHint,
 }) {
   const size = "large";
-  const maxLength = "1000";
-  const TASK_INPUT_MSG = {
-    taskInputQuestion: "What are the details?",
-    taskInputHint: "Be as specific as you can about what needs doing",
-    errorHint: "Please enter at least 25 characters and a maximum of 1000 ",
-  }
+  const maxLength = 1000;
+  const taskInputQuestion = "What are the details?";
+  const taskInputHint = "Be as specific as you can about what needs doing";
+
   return (
-    <TaskInput
+    <React.Fragment>
+    <h2 className={styles.other_heading}> 
+      {taskInputQuestion}
+    </h2>
+    <label className={styles.hint_label}> 
+      {taskInputHint}
+    </label>
+    <FormTextAreaInput
       isAbleToSubmitTaskDescription={isJobDetailsValid}
       displayValue={jobDetails}
       size={size}
       maxLength={maxLength}
       onJobInputChange={onJobDetails}
-      taskInputMsg={TASK_INPUT_MSG}
+      errorHint={errorHint}
     />
+    </React.Fragment>
       )
   }
 

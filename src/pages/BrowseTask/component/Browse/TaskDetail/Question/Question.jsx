7a56a037f460eb4867ce28824501dc6adcc8a707
Ziwei Lan <ziweilan92@gmail.com>
2020-09-17 22:13:08 +1000

    use FormTextAreaInput in Question component

--- src/pages/BrowseTask/component/Browse/TaskDetail/Question/Question.jsx
+++ src/pages/BrowseTask/component/Browse/TaskDetail/Question/Question.jsx
@@ -2,12 +2,10 @@ import React, { useState } from 'react';
 
 import styles from './Question.module.scss';
 
+import FormTextAreaInput from '../../../../../../components/FormTextAreaInput';
+
 function Question() {
   const [input, updateInput] = useState('');
-
-  function handleChange(e) {
-    updateInput(e.target.value);
-  }
 
   const title = 'QUESTION';
   const notice = `Please don't share personal info – insurance won't apply to tasks done privately!`;
@@ -26,11 +24,11 @@ function Question() {
       <div className = {styles.input_wrapper}>
         <div className = {styles.avatar}/>
         <div className = {styles.input}>
-          <textarea 
-            className = {styles.text_area}
-            placeholder = {placeHolder}
-            value = {input}
-            onChange = {handleChange}
+          <FormTextAreaInput
+            size={'large'}
+            placeHolder={placeHolder}
+            displayValue={input}
+            onInputChange={updateInput}
           />
         </div>
       </div>

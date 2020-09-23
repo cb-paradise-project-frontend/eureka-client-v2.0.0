355b3063bd9455460c83aa03ddbc5bc24420af28
Ziwei Lan <ziweilan92@gmail.com>
2020-09-23 21:22:39 +1000

    remove default logic for close button on ModalTest

--- src/components/ModalTest/Modal.jsx
+++ src/components/ModalTest/Modal.jsx
@@ -1,29 +1,20 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 
 import styles from './Modal.module.scss';
 
 import Overlay from '../Overlay';
 import CloseIconButton from '../CloseIconButton';
 
-const Modal = ({ onCloseClick, children }) => {
-  const history = useHistory();
-
-  const closeModal = history.goBack;
-
-  const onRequestClose = onCloseClick || closeModal;
-
-  return (
-    <Overlay>
-      <div className={styles.modal_container} >
-        <div className={styles.close_button} >
-          <CloseIconButton onClick={onRequestClose} />
-        </div>
-        {children}
+const Modal = ({ onRequestClose, children }) => (
+  <Overlay>
+    <div className={styles.modal_container} >
+      <div className={styles.close_button} >
+        <CloseIconButton onClick={onRequestClose} />
       </div>
-    </Overlay>
-  );
-};
+      {children}
+    </div>
+  </Overlay>
+);
 
 const Header = ({ children }) => (
   <div className={styles.header} >

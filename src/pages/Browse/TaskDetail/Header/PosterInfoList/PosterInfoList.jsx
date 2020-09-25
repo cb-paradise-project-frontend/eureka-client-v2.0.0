9320baadd3dd82e8cd8e9fbbe77a8eab99394ed2
Ziwei Lan <ziweilan92@gmail.com>
2020-09-25 17:29:18 +1000

    combine PosterInfoItem into PosterInfoList

--- src/pages/Browse/TaskDetail/Header/PosterInfoList/PosterInfoList.jsx
+++ src/pages/Browse/TaskDetail/Header/PosterInfoList/PosterInfoList.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import classNames from 'classnames/bind';
 
 import styles from './PosterInfoList.module.scss';
+import Avatar from '../../../../../components/Avatar';
 
-import PosterInfoItem from './PosterInfoItem';
+const cx = classNames.bind(styles);
 
 function PosterInfoList({ posterInfo }) {
+  const PosterInfoItem = ({ title, content }) => {
+    const displayContent = content.name || content;
+
+    return (
+      <div className = {styles.poster_info_item}>
+        <div className = {styles.icon_wrapper}>
+          <div className = {cx(
+            {
+              'POSTED BY': 'avatar_wrapper',
+              'LOCATION': 'location_icon',
+              'DUE DATE': 'date_icon',
+            }[title]
+          )}>
+            {(title === 'POSTED BY') &&
+              <Avatar avatarUrl={content.avatar} />
+            }
+          </div>
+        </div>
+        <div className = {styles.poster_detail}>
+          <div className = {styles.title}>
+            {title}
+          </div>
+          <div className = {styles.content}>
+            {displayContent}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   const titleList = ['POSTED BY', 'LOCATION', 'DUE DATE'];
 
   const posterInfoList = titleList.map((title, index) =>
@@ -14,7 +46,7 @@ function PosterInfoList({ posterInfo }) {
       content = {posterInfo[index]}
     />
   );
-  
+
   return (
     <div className = {styles.poster_info}>
       {posterInfoList}

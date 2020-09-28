fd6a05a0cebd0ad44872ef3abed8e8a67d4ee39e
Ziwei Lan <ziweilan92@gmail.com>
2020-09-28 22:12:35 +1000

    add loadProfile logic to reducer

--- src/pages/CreateProfile/Reducer/profileReducer.js
+++ src/pages/CreateProfile/Reducer/profileReducer.js
@@ -4,7 +4,9 @@ export default function profileReducer(state, action) {
   console.log(action);
   switch (action.type) {
     case ACTION.LOAD_PROFILE:
+      console.log(action.payload.userProfile);
       return {
+        ...state,
         ...action.payload.userProfile,
       };
     case ACTION.PHOTO_UPLOAD:
@@ -12,30 +14,35 @@ export default function profileReducer(state, action) {
         ...state,
         photo: action.payload.photo,
         subPage: '',
+        autoSave: true,
       };
     case ACTION.ACCOUNT_INPUT:
       return {
         ...state,
         bankAccount: action.payload.bankAccount,
         subPage: '',
+        autoSave: true,
       };
     case ACTION.BILLING_ADDRESS_INPUT:
       return {
         ...state,
         billingAddress: action.payload.billingAddress,
         subPage: '',
+        autoSave: true,
       };
     case ACTION.BIRTHDAY_INPUT:
       return {
         ...state,
         birthday: action.payload.birthday,
         subPage: '',
+        autoSave: true,
       };
     case ACTION.MOBILE_INPUT:
       return {
         ...state,
         mobile: action.payload.mobile,
         subPage: '',
+        autoSave: true,
       };
     case ACTION.CLICK_PROFILE_ITEM:
       return {

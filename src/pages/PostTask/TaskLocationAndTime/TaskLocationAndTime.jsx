ec692e7f2e20f517debb7c9365b6ed50786e994f
Changhong Lin <changhong.linst@gmail.com>
2020-09-29 07:04:43 +1000

    add google place autocomplete

--- src/pages/PostTask/TaskLocationAndTime/TaskLocationAndTime.jsx
+++ src/pages/PostTask/TaskLocationAndTime/TaskLocationAndTime.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 
 import styles from '../PostTask.module.scss';
+import styled from 'styled-components';
 
 import TaskDeliveryMethod from './TaskDeliveryMethod';
-import SuburbInput from '../../../../api/SuburbInput';
+import Place from '../../../utils/getLocation';
 
 function TaskLocationAndTime({
   taskDatePicker
@@ -12,22 +13,26 @@ function TaskLocationAndTime({
  //webpack 报的错，没有声明，说明什么，webpack不是默认拿全局，use strict，通过window拿全局
 
  //callback fn， 每一次组件render，会call useEffect里面的callback fn
- useEffect(() => { //lifecycle 
-  initialize("regions");
-  //componentwillupmount
- }, [initialize]) //dependency, dependency改变才会刷新，两个同时match才会callback fn， dependency 如果是个变值，相当于compenentdidupdate
+//  useEffect(() => { //lifecycle 
+//   initialize("regions");
+//   //componentwillupmount
+//  }, [initialize]) //dependency, dependency改变才会刷新，两个同时match才会callback fn， dependency 如果是个变值，相当于compenentdidupdate
 
- function initialize(type) {
-  const options = {
-    componentRestrictions: {country: 'au'},
-    types: [`(${type})`]
-  };
+//  function initialize(type) {
+//   const options = {
+//     componentRestrictions: {country: 'au'},
+//     types: [`(${type})`]
+//   };
 
-  const input = document.getElementById('searchTextField');
-  const autocomplete = new window.google.maps.places.Autocomplete(input, options);
+//   const input = document.getElementById('searchTextField');
+//   const autocomplete = new window.google.maps.places.Autocomplete(input, options);
  
-}
+// }
+const [ method, setMethod ] = useState(true);
 
+const handleClick = () => {
+  setMethod(!method);
+}
 
   return (
     <React.Fragment>
@@ -35,8 +40,10 @@ function TaskLocationAndTime({
         <h2 className={styles.other_heading}> 
           Where do you need it done? 
         </h2>
-        <TaskDeliveryMethod />
-        <input id="searchTextField" type="text" />
+        <TaskDeliveryMethod handleClick={handleClick} />
+        <div className={styles.date_box}>
+        {method && <Place />}
+        </div>
         <h2 className={styles.other_heading}> 
           When do you need it done? 
         </h2>

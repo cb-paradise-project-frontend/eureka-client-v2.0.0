84bd5f806678dd2c4b0cb0effbab2b45c2c1a246
Simon Yu <simon94hyu@gmail.com>
2020-09-29 17:23:35 +0930

    put all the data to landing

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -9,19 +9,103 @@ import Articles from "./Articles";
 import TopCategories from "./TopCategories";
 import Footer from "./Footer";
 
-function Landing() {
-  return (
-    <>
+class Landing extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        article:
+        [
+          {
+            id: 1,
+            articleImg: require("../../assets/garden-party.png"),
+            articleTitle: "How to paint your kitchen cabinets",
+            articleContent:
+              "Looking to refresh the kitchen? It’s easier than you think…",
+          },
+          {
+            id: 2,
+            articleImg: require("../../assets/beautiful-backyard.png"),
+            articleTitle: "Bedroom plants for your room",
+            articleContent:
+              "Create your own lush, indoor jungle with plenty of potted greens!",
+          },
+          {
+           id: 3,
+            articleImg: require("../../assets/bedroom-plant.png"),
+            articleTitle: "35 Dreamy boho bedroom ideas",
+            articleContent:
+              "Take your bedroom from basic to bohemian with these fabulous colourful ideas!",
+          },
+        ],
+
+        tasker: [
+          {
+            img: {
+              taskerImg: require("../../assets/tasker-samantha.png"),
+            },
+            profile: {
+              name: "Samantha",
+              spec: "Specialities: assembly, pet care, gardening",
+              details:
+                "Returning to the workforce as a single mum, Sam had to find something that could be flexible and cover the cost of childcare.",
+              stars: "4.9 stars from 185 review",
+              reviews:
+                "Very nice and caring in trying circumstances! Thanks again",
+              viewer: "---Tim S.",
+            },
+          },
+          {
+            img: {
+              taskerImg: require("../../assets/tasker-Emily.png"),
+            },
+            profile: {
+              name: "Emily",
+              spec: "Specialities: delivery, cleaning, packing",
+              details:
+                "In-between jobs, Emily was looking for a way to earn some extra cash... Maybe even using her clown school skills!",
+              stars: "5 stars from 6 reviews",
+              reviews:
+                "She was an absolute lifesaver. Quick, friendly and super efficient!",
+              viewer: "---Myles B.",
+            },
+          },
+          {
+            img: {
+              taskerImg: require("../../assets/tasker-Brandon.png"),
+            },
+            profile: {
+              name: "Brandan",
+              spec:
+                "She was an absolute lifesaver. Quick, friendly and super efficient!",
+              details:
+                "A sparky by trade, Brendon jumped onboard when he went back to studying. Here's how Airtasker fit in with his busy lifestyle...",
+              stars: "5 stars from 6 reviews",
+              reviews:
+                "Nice work and will use Brendon again if any other lighting task",
+              viewer: "---Steven Z.",
+            },
+          },
+        ],
+
+      
+    };
+  }
+
+  render() {
+    return (
+      <>
       <Home />
       <TaskCategories />
       <OtherJobs />
       <TopCategories />
-      <MeetTasker />
+      <MeetTasker data={this.state.tasker}/>
       <ThingsToKnow />
-      <Articles />
+      <Articles data={this.state.article}/>
       <Footer />
     </>
-  );
+    );
+  }
 }
 
 export default Landing;

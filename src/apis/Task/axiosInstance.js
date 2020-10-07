34838fe1c43acae4af4515d333ad8dd8a57da457
Ziwei Lan <ziweilan92@gmail.com>
2020-10-07 18:40:50 +1100

    extract axios instance to Task folder

---
+++ src/apis/Task/axiosInstance.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const taskData = axios.create({
+  baseURL: 'http://localhost:5000/api/tasks',
+});
+
+export default taskData;
--- src/apis/Task/getTaskList.js
+++ src/apis/Task/getTaskList.js
@@ -1,8 +1,4 @@
-import axios from 'axios';
-
-const taskData = axios.create({
-  baseURL: 'http://localhost:5000/api/tasks',
-});
+import taskData from './axiosInstance';
 
 export default function getTaskList() {
   return taskData.get();

2f38b670f8a69debb490635710f3223ed1c5fcc7
Lucas Guo <gjn1738051666@gmail.com>
2020-10-10 00:37:32 +1000

    responsive version 1.0

--- src/pages/Landing/OtherJobs/components/OtherJobsNav/OtherJobsNav.jsx
+++ src/pages/Landing/OtherJobs/components/OtherJobsNav/OtherJobsNav.jsx
@@ -15,6 +15,7 @@ class OtherJobsNav extends Component {
 
     this.handleComponentShowing = this.handleComponentShowing.bind(this);
     this.handleShowingBlock = this.handleShowingBlock.bind(this);
+    // this.result = this.result.bind(this);
   }
 
   handleShowingBlock(index) {
@@ -24,62 +25,18 @@ class OtherJobsNav extends Component {
   }
   
   handleComponentShowing(index) {
-    switch(index) {
-      case("Moving home"): 
-        return(
-          <OtherJobsCardContainer
-          tabDescription = "Got a few boxes to shift, an apartment or entire house? Get your home moved just the way you want, by whom you want, when you want. Let Airtasker shoulder the load."
-          title = "DELVERY" 
-          description = "Deliver 1,000 letterbox flyers in Kellyville"
-          price = "$50"
-          rate = "5 Stars"
-          />
-        );
-      case("Starting a business"): 
-        return(
-          <OtherJobsCardContainer
-          tabDescription = "Taking a big leap and need some expert advice or assistance? Airtasker can help you get some cracking marketing collateral, admin muscle or a few extra hands to help ease the burden."
-          title = "DESIGN" 
-          description = "Logo designer for photography business"
-          price = "$88"
-          rate = "5 Stars"
-          />
-        );
-      case("Fixing stuff"): 
-        return(
-          <OtherJobsCardContainer
-          tabDescription = "Do you have a hole in the wall that needs plugging? Perhaps a TV that needs mounting? Or maybe you have that perfect shade of green, but no time to paint? Get a Tasker to help."
-          title = "HANDYMAN / TRADIES" 
-          description = "Help assemble a flat pack."
-          price = "$30"
-          rate = "5 Stars"
-          />
-        );
-      case("Hosting a party"): 
-        return(
-          <OtherJobsCardContainer
-          tabDescription = "Got something to celebrate and the guest list all ready, but need everything else? Let Airtasker help you find the best bartenders, party planners, photographers and entertainment in the land."
-          title = "CATERING" 
-          description = "Help With A Launch Party -- This Thursday"
-          price = "$100"
-          rate = "5 Stars"
-          />
-        );
-      case("Something different"): 
-        return(
-          <OtherJobsCardContainer
-          tabDescription = "Want something done but feel like it’s too random? Whether it’s getting rescued from a spider or help building a bobsled - you can get nearly anything done through Airtasker."
-          title = "CHEF" 
-          description = "Teach me your family pasta sauce recipe"
-          price = "$575"
-          rate = "5 Stars"
-          />
-        );
-      default: 
-      return (
-        <h1>something wrong</h1>
-      )
-    }
+    const result = this.props.data.filter(item => item.id === index).map((item, i) => {
+        return <OtherJobsCardContainer
+        tabDescription = {item.tabDescription}
+        title = {item.title}
+        description = {item.description}
+        price = {item.price}
+        rate = {item.rate}
+        />;
+    })
+    
+
+    this.setState({selectCard:result[0]}) ;
   }
 
   render () {
@@ -88,16 +45,19 @@ class OtherJobsNav extends Component {
     return (
       <div className={styles.OtherJobs__container}>
           <div className={styles.OtherJobs__title}>
-            <ShowCard onClick={this.handleShowingBlock}>Moving home</ShowCard>
-            <ShowCard onClick={this.handleShowingBlock}>Starting a business</ShowCard>
-            <ShowCard onClick={this.handleShowingBlock}>Fixing stuff</ShowCard>
-            <ShowCard onClick={this.handleShowingBlock}>Hosting a party</ShowCard>
-            <ShowCard onClick={this.handleShowingBlock}>Something different</ShowCard>
+            <ShowCard onClick={this.handleComponentShowing}>Moving home</ShowCard>
+            <ShowCard onClick={this.handleComponentShowing}>Starting a business</ShowCard>
+            <ShowCard onClick={this.handleComponentShowing}>Fixing stuff</ShowCard>
+            <ShowCard onClick={this.handleComponentShowing}>Hosting a party</ShowCard>
+            <ShowCard onClick={this.handleComponentShowing}>Something different</ShowCard>
           </div>
 
           <div>
             {
-              this.handleComponentShowing(currentComponentIndex)
+              // this.result(currentComponentIndex)
+              // this.handleComponentShowing(currentComponentIndex)
+              
+              this.state.selectCard
             }
           </div>
 

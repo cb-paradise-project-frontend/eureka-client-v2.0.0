97a2dd07964a6956bf38188906e862e58fc3cf10
Ziwei Lan <ziweilan92@gmail.com>
2020-10-10 23:31:14 +1100

    simplify mapping logic

--- src/pages/Browse/TaskDetail/Header/TaskInfo/TaskInfo.jsx
+++ src/pages/Browse/TaskDetail/Header/TaskInfo/TaskInfo.jsx
@@ -7,67 +7,57 @@ import { TaskContext } from '../../../TaskContext';
 
 const cx = classNames.bind(styles);
 
-function TaskInfo() {
-  const { postedBy, location, due } = useContext(TaskContext);
-
-  const TaskInfoItem = ({ title, content }) => {
-    const displayContent = content.name || content;
-
-    return (
-      <div className = {styles.poster_info_item}>
-        <div className = {styles.icon_wrapper}>
-          <div className = {cx(
-            {
-              'POSTED BY': 'avatar_wrapper',
-              'LOCATION': 'location_icon',
-              'DUE DATE': 'date_icon',
-            }[title]
-          )}>
-            {(title === 'POSTED BY') &&
-              <Avatar avatarUrl={content.avatar} />
-            }
-          </div>
-        </div>
-        <div className = {styles.poster_detail}>
-          <div className = {styles.title}>
-            {title}
-          </div>
-          <div className = {styles.content}>
-            {displayContent}
-          </div>
-        </div>
-      </div>
-    );
-  };
-
-  const taskInfoList = [
-    {
-      title: 'POSTED BY',
-      data: postedBy,
-    },
-    {
-      title: 'LOCATION',
-      data: location,
-    },
-    {
-      title: 'DUE DATE',
-      data: due,
-    },
-  ];
-
-  const displayedTaskInfo = taskInfoList.map(({ title, data }) =>
-    <TaskInfoItem
-      key = {title}
-      title = {title}
-      content = {data}
-    />
-  );
+function TaskInfoItem({ title, content }) {
+  const displayContent = content.name || content;
 
   return (
-    <div className = {styles.poster_info}>
-      {displayedTaskInfo}
+    <div className={styles.poster_info_item} >
+      <div className={styles.icon_wrapper} >
+        <div className={cx(
+          {
+            'POSTED BY': 'avatar_wrapper',
+            'LOCATION': 'location_icon',
+            'DUE DATE': 'date_icon',
+          }[title],
+        )}>
+          {(title === 'POSTED BY') &&
+            <Avatar avatarUrl={content.avatar} />
+          }
+        </div>
+      </div>
+      <div className={styles.poster_detail} >
+        <div className={styles.title} >
+          {title}
+        </div>
+        <div className={styles.content} >
+          {displayContent}
+        </div>
+      </div>
     </div>
   );
 }
 
-export default TaskInfo;
+export default function TaskInfo() {
+  const { postedBy, location, due } = useContext(TaskContext);
+
+  const taskInfo = { postedBy, location, due };
+  const infoTitle = {
+    postedBy: 'POSTED BY',
+    location: 'LOCATION',
+    due: 'DUE DATE',
+  };
+
+  const displayedTaskInfo = Object.keys(taskInfo).map((key) => (
+    <TaskInfoItem
+      key={key}
+      title={infoTitle[key]}
+      content={taskInfo[key]}
+    />
+  ));
+
+  return (
+    <div className={styles.poster_info} >
+      {displayedTaskInfo}
+    </div>
+  );
+}

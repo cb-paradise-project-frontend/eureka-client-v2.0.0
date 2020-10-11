3d45f3a4eb5589886d80462d8f04f2aff3e31866
Simon Yu <simon94hyu@gmail.com>
2020-10-11 15:31:13 +1030

    responsive second update

--- src/pages/Landing/ThingsToKnow/FeatureBlock/FeatureContent/FeatureContent.jsx
+++ src/pages/Landing/ThingsToKnow/FeatureBlock/FeatureContent/FeatureContent.jsx
@@ -4,12 +4,12 @@ import styles from "./FeatureContent.module.scss";
 
 function FeatureContent(props){
     return(
-        <div className={styles.FeatureContent}>
+        <div className={styles.feature_content}>
         <div className={styles.subhead}>{props.data.subhead}</div>
         <div className={styles.describe}>{props.data.describe}</div>
         <a
           href="https://support.airtasker.com/hc/en-au/categories/200049939-Payments"
-          className={styles.readMore}
+          className={styles.read_more}
         >
           Read more
         </a>

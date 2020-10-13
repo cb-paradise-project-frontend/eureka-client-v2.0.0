98955552ed17f35198ce903073badc2da01143c3
TTCode10 <ttcode10@gmail.com>
2020-10-13 08:06:32 +0800

    ♻️ Refactor authProvider to pull user from token

--- src/auth/Auth/AuthProvider.js
+++ src/auth/Auth/AuthProvider.js
@@ -1,48 +1,29 @@
-import React, {useState, useEffect} from 'react';
-import { auth } from './../../firebase';
+import React, { useState, useEffect } from 'react';
+// eslint-disable-next-line camelcase
+import jwt_decode from 'jwt-decode';
 import AuthContext from './AuthContext';
 
-const AuthProvider = ({children}) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const [authModalVisible, setAuthModalVisible] = useState({
-    loginModal: false,
-    signupModal: false,
-  })
+const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState({
+    currentUser: null,
+  });
 
-  const showLoginModal = () => {
-    setAuthModalVisible({
-      loginModal: true,
-      signupModal: false,
-    })
-  }
-
-  const showSignupModal = () => {
-    setAuthModalVisible({
-      loginModal: false,
-      signupModal: true,
-    })
-  }
-
-  const hideAuthModal = () => {
-    setAuthModalVisible({
-      loginModal: false,
-      signupModal: false,
-    })
-  }
+  const extractUserFromToken = () => {
+    const token = localStorage.getItem('token');
+    if (!token) return;
+    const decoded = jwt_decode(token);
+    setCurrentUser(decoded);
+  };
 
   useEffect(() => {
-    auth.onAuthStateChanged((user) => {
-      console.log('user in auth', user);
-      console.log('Auth provider', authModalVisible);
-      setCurrentUser(user);
-    });
-  }, [authModalVisible]);
+    extractUserFromToken();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ currentUser }}>
       {children}
     </AuthContext.Provider>
   );
-}
+};
 
 export default AuthProvider;

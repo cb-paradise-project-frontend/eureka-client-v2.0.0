16101ccca7b30cadd5c07ed5b30519dd5ed35259
Ziwei Lan <ziweilan92@gmail.com>
2020-10-13 17:34:26 +1100

    refactor getTaskList for more filtering functions

--- src/apis/getTaskList.js
+++ src/apis/getTaskList.js
@@ -1,9 +1,21 @@
 import taskData from './axiosInstance';
 
-export default function getTaskList(keyword) {
-  const config = keyword
-    ? `?keyword=${keyword}`
-    : '';
+export default function getTaskList(filter) {
+  const filterKeyArray = filter && Object.keys(filter);
 
-  return taskData.get(config);
+  const filteredConfig = filterKeyArray && filterKeyArray
+    .filter((key) => filter[key]);
+
+  if (!filteredConfig) return taskData.get();
+
+  const urlConfig = filteredConfig.reduce((url, key, index) => (
+    index === filteredConfig.length - 1
+      ? `${url}${key}=${filter[key]}`
+      : `${url}${key}=${filter[key]}&`
+  ), '?');
+
+  // const config = stringConfig
+  //   .slice(0, stringConfig.length - 1);
+
+  return taskData.get(urlConfig);
 }

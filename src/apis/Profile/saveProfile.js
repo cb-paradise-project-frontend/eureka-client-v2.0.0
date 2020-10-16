bcef798de730c3fa173099f9de9d94db253a124b
Ziwei Lan <ziweilan92@gmail.com>
2020-10-16 16:12:15 +1100

    add saveProfile api

---
+++ src/apis/Profile/saveProfile.js
@@ -0,0 +1,48 @@
+import axiosInstance from '../axiosInstance';
+
+export default async function saveProfile(userId, profile) {
+  const {
+    bankAccount: {
+      holder,
+      accountNumber,
+      bsb,
+    },
+    billingAddress: {
+      lineOne,
+      lineTwo,
+      suburb,
+      state,
+      postcode,
+      country,
+    },
+    birthday: {
+      day,
+      month,
+      year,
+    },
+    mobile,
+  } = profile;
+
+  const formatData = {
+    accountHolder: holder,
+    user: userId,
+    accountNumber,
+    bsb,
+    billingAddress: {
+      lineOne,
+      lineTwo,
+      suburb,
+      state,
+      postcode,
+      country,
+    },
+    birthday: new Date(year, month - 1, day),
+    mobile,
+  };
+
+  const response = await axiosInstance.put('/profiles', formatData);
+
+  console.log(response);
+
+  return response;
+}
--- src/apis/index.js
+++ src/apis/index.js
@@ -3,4 +3,4 @@ export { default as askQuestion } from './Task/askQuestion';
 export { default as getTaskList } from './Task/getTaskList';
 export { default as useAuth } from './useAuth';
 export { default as getProfile } from './Profile/getProfile';
-
+export { default as saveProfile } from './Profile/saveProfile';

ca30cbd7dcbada74b128d8ca26d9705f0ea005de
Ziwei Lan <ziweilan92@gmail.com>
2020-10-18 00:47:33 +1100

    add makeOffer api function

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,6 +1,7 @@
 export { default as api } from './axiosInstance';
 export { default as askQuestion } from './Task/askQuestion';
 export { default as getTaskList } from './Task/getTaskList';
+export { default as makeOffer } from './Task/makeOffer';
 export { default as useAuth } from './useAuth';
 export { default as getProfile } from './Profile/getProfile';
 export { default as saveProfile } from './Profile/saveProfile';

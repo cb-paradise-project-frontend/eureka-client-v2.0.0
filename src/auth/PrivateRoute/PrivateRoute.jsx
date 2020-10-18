42409a61f8ff888a0c9075d36355a87edb41738b
TTCode10 <ttcode10@gmail.com>
2020-10-18 10:26:01 +0800

    ♻️ Pan: Refactored private route

--- src/auth/PrivateRoute/PrivateRoute.jsx
+++ src/auth/PrivateRoute/PrivateRoute.jsx
@@ -1,10 +1,13 @@
 import React, { useContext, useEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { AuthContext } from './../Auth';
+import LoginModal from './../../components/LoginModal';
+import { useToggleContent } from './../../components/ToggleContent';
 
 const PrivateRoute = ({component, ...restProps}) => {
   const RouteComponent = component;
   const { currentUser } = useContext(AuthContext);
+  const [Content, toggler] = useToggleContent(true);
 
   useEffect(() => {
     console.log('current user in private route', currentUser);
@@ -13,11 +16,13 @@ const PrivateRoute = ({component, ...restProps}) => {
   return (
     <Route
       {...restProps}
-      render={(routeProps) => 
+      render={(routeProps) =>
         !!currentUser ? (
           <RouteComponent {...routeProps} />
         ) : (
-          <Redirect to={'/login'} />
+          <Content>
+            <LoginModal pageToggler={toggler} />
+          </Content>
         )
       }
     />

2271421f1429a6c392ca184cbe46e8172e917513
TTCode10 <ttcode10@gmail.com>
2020-10-20 02:11:55 +0800

    ✨ Pan / Get auth with backend when refresh page

--- src/apis/Auth/checkUser.js
+++ src/apis/Auth/checkUser.js
@@ -1,7 +1,13 @@
 import { api } from './../axiosInstance';
+import { removeLocalToken } from './../utils';
 
-const checkUser = () => {
-  api.get('/auth');
+const checkUser = async () => {
+  try {
+    await api.get('/auth');
+  } catch (error) {
+    console.log(error);
+    removeLocalToken();
+  }
 };
 
 export default checkUser;
--- src/auth/Auth/AuthProvider.js
+++ src/auth/Auth/AuthProvider.js
@@ -2,32 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 // eslint-disable-next-line camelcase
 import AuthContext from './AuthContext';
-import { checkUser, extractInfoFromToken, checkTokenExpiry, removeLocalToken } from './../../apis';
+import { checkUser, extractInfoFromToken } from './../../apis';
 
 const AuthProvider = ({ children }) => {
-  const history = useHistory();
+  // const history = useHistory();
   const [currentUser, setCurrentUser] = useState(null);
 
-  const checkTokenValidity = () => {
-    const decoded = extractInfoFromToken();
-    if (!decoded) return;
-    const result = checkTokenExpiry(decoded);
-    if (!result) {
-      removeLocalToken();
-      history.push('/');
-    } return decoded;
-  }
-
   const setUser = (decodedUser) => {
-    if (!decodedUser) return;
     const { userId, firstName, lastName, email } = decodedUser;
     setCurrentUser({userId, firstName, lastName, email});
   }
 
   useEffect(() => {
-    checkTokenValidity();
     checkUser();
-    setUser();
+    const decoded = extractInfoFromToken();
+    if (!decoded) return;
+    setUser(decoded.user);
   }, []);
 
   useEffect(() => {

39a42225c372c0ef75d94ce89fab48c4917de479
Ziwei Lan <ziweilan92@gmail.com>
2020-10-20 13:47:25 +1100

    refactor toggling logic of useLoadingPage

--- src/components/LoadingPage/useLoadingPage/useLoadingPage.jsx
+++ src/components/LoadingPage/useLoadingPage/useLoadingPage.jsx
@@ -5,11 +5,15 @@ import LoadingPage from '../LoadingPage';
 export default function useLoadingPage(initialState = true) {
   const [loading, toggleLoading] = useState(initialState);
 
+  const toggler = () => (
+    toggleLoading((prevState) => !prevState)
+  );
+
   const Content = ({ children }) => (
     <>
       {loading ? <LoadingPage /> : children}
     </>
   );
 
-  return [Content, toggleLoading, loading];
+  return [Content, toggler, loading];
 }
--- src/pages/Browse/Browse.jsx
+++ src/pages/Browse/Browse.jsx
@@ -73,7 +73,7 @@ export default function Browse() {
 
   useEffect(() => {
     if (taskList.length && loadingStatus === true) {
-      toggleLoadingMask(false);
+      toggleLoadingMask();
     }
   }, [taskList.length, loadingStatus]);
 

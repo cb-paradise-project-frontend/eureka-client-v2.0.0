ceb87723fa54fe0721fd18f7bf46eca4961d9b08
Wenpei Zhang <67667224+JavaScriptN0ob@users.noreply.github.com>
2020-10-20 16:48:55 +1100

    Revert "Changhong/post task backend connection"

--- src/apis/utils/extractInfoFromToken.js
+++ src/apis/utils/extractInfoFromToken.js
@@ -1,7 +1,7 @@
 import jwt_decode from 'jwt-decode';
 
 const extractInfoFromToken = () => {
-  const token = window.localStorage.getItem('token');
+  const token = localStorage.getItem('token');
   if (!token) return;
   const decoded = jwt_decode(token);
   return decoded;

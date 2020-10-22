947249e245c549268667affb6d93cf578b7e103b
Ziwei Lan <ziweilan92@gmail.com>
2020-10-22 21:31:12 +1100

    refactor toDate

--- src/pages/OfferModal/ProfilePage/SubPages/Birthday/utils/toDate.js
+++ src/pages/OfferModal/ProfilePage/SubPages/Birthday/utils/toDate.js
@@ -1,4 +1,5 @@
 export default function toDate(birthdayObj) {
   const { day, month, year } = birthdayObj;
-  return new Date(year, month - 1, day);
+
+  return new Date(year, month - 1, day).toString();
 }

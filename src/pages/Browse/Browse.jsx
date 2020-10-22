e1faaa6fca0fffb9189e0cde4f3e23df64b10072
Ziwei Lan <ziweilan92@gmail.com>
2020-10-22 22:09:57 +1100

    remove local test data

--- src/pages/Browse/Browse.jsx
+++ src/pages/Browse/Browse.jsx
@@ -7,41 +7,7 @@ import getTaskList from '../../apis/Task/getTaskList';
 import TaskMenu from './TaskMenu';
 import useLoadingPage from '../../components/LoadingPage/useLoadingPage';
 import { LoadTaskProvider } from './TaskDetail/LoadTaskContext';
-import EmptyTask from './EmptyTask/EmptyTask';
-
-const testData = {
-  title: 'Roof repair',
-  status: 'OPEN',
-  budget: '120',
-  postedBy: {
-    name: 'Tifa',
-    avatar: '',
-  },
-  location: 'Kurunjang VIC 3337, Australia',
-  due: 'Monday, 24th Aug 2020',
-  description: `- remove existing bamboo fence - supply and install new fence (open for ideas)
-- install synthetic turf
-- landscaping design and install
-- remove and upgrade existing waterfall feature into the pool`,
-  comments: [],
-};
-
-function createData(size, data) {
-  const dataArray = [];
-  for (let i = 0; i < size; i += 1) {
-    const newData = { ...data };
-    newData.id = `${i}`;
-    newData.questions = [];
-    dataArray.push(newData);
-  }
-  return dataArray;
-}
-
-const tasks = createData(10, testData);
-tasks[1].title = 'Wall repair';
-tasks[1].status = 'ASSIGNED';
-tasks[2].status = 'COMPLETED';
-tasks[3].status = 'EXPIRED';
+import EmptyTask from './EmptyTask';
 
 export default function Browse() {
   const [taskList, setTaskList] = useState([]);

6647529ce1d1c2b2f3ceeb8ce7f1a07bfd1c7eb6
TTCode10 <ttcode10@gmail.com>
2020-10-22 22:48:14 +0800

    💄 Pan / Style log out button with Styled-Components

--- src/components/Navigation/components/LogOut/LogOut.jsx
+++ src/components/Navigation/components/LogOut/LogOut.jsx
@@ -2,7 +2,26 @@ import React, { useContext } from 'react';
 import { AuthContext } from '../../../../auth/Auth';
 import { useHistory } from 'react-router-dom';
 import { removeLocalToken } from '../../../../apis/utils';
+import styled from 'styled-components';
 
+const LogOutButton = styled.button`
+  color:#426ad2;
+  text-align: center;
+  cursor: pointer;
+  display: block;
+  font-size: 13px;
+  letter-spacing: .4px;
+  text-decoration: none;
+  border: none;
+  outline: none;
+  font-weight: 600;
+  background-color: transparent;
+  margin: 0 15px;
+
+  &:hover {
+    color: #008fb4;
+  }
+`
 
 const LogOut = () => {
   const { setUser } = useContext(AuthContext);
@@ -15,7 +34,7 @@ const LogOut = () => {
   }
 
   return (
-    <button onClick={onLogOut}>Log Out</button>
+    <LogOutButton onClick={onLogOut}>Log Out</LogOutButton>
   )
 }
 

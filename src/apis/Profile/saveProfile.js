3dc38909d6da3e3839705f94ca3ab728fb450946
Ziwei Lan <ziweilan92@gmail.com>
2020-10-22 18:45:57 +1100

    refactor saveProfile to fit backend update

--- src/apis/Profile/saveProfile.js
+++ src/apis/Profile/saveProfile.js
@@ -2,19 +2,7 @@ import { api } from '../axiosInstance';
 import { destructure } from '../utils';
 
 export default async function saveProfile(profile) {
-  const { birthday: {
-      day,
-      month,
-      year,
-    },
-  } = profile;
-
-  const formatData = {
-    ...profile,
-    birthday: new Date(year, month - 1, day),
-  };
-
-  const response = await api.put('/profiles', formatData);
+  const response = await api.put('/profiles', profile);
 
   return (response.status === 200) && destructure(response);
 }

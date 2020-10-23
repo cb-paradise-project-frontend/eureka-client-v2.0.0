1e86f83a9ec1ae81709fb8324841a7d7ab4a4e19
Wayne JSNoob <wenpeijs@gmail.com>
2020-10-23 22:38:03 +1100

    ✨ added reset password functionality when user know their current password

--- src/apis/Profile/updateUserName.js
+++ src/apis/Profile/updateUserName.js
@@ -1,5 +1,4 @@
 import { api } from '../axiosInstance';
-import { destructure } from '../utils';
 
 const updateUserName = async (username) => {
   const response = await api.put('/users/name', username);
--- src/pages/Profile/ProfileContent/Password/Password.jsx
+++ src/pages/Profile/ProfileContent/Password/Password.jsx
@@ -4,16 +4,19 @@ import styles from './Password.module.scss';
 
 import Input from '../../../../components/Input';
 import Button from '../../../../components/Button';
+import resetPassword from '../../../../apis/Profile/resetPassword';
 
 class Password extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       currentPassword: '',
       newPassword: '',
       repeatPassword: '',
     };
+
+    this.handleResetPassword = this.handleResetPassword.bind(this);
   }
 
   handleInputChangeCreator = (key) => (event) => {
@@ -25,6 +28,14 @@ class Password extends React.Component {
     }));
   }
 
+  handleResetPassword = async () => {
+    const { currentPassword, newPassword } = this.state;
+    const passwords = { currentPassword, newPassword };
+
+    const res = resetPassword(passwords);
+    console.log(res);
+  }
+
   render() {
     const passwordComponent = Object.keys(this.state).map((title) => {
       const value = this.state[title];
@@ -34,18 +45,20 @@ class Password extends React.Component {
           key={title}
           label={title}
           value={value}
+          type="password"
           onChange={this.handleInputChangeCreator(title)}
         />
-      )
-    })
+      );
+    });
+
     return (
       <div className={styles.password_wrapper}>
         {passwordComponent}
         <div className={styles.update_btn}>
-          <Button>Reset Password</Button>
+          <Button onClick={this.handleResetPassword}>Reset Password</Button>
         </div>
       </div>
-    )
+    );
   }
 }
 

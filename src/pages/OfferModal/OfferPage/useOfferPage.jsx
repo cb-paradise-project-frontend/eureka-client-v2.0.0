9f6b85449c763da67b0c240d9712cb5965598cd8
Ziwei Lan <ziweilan92@gmail.com>
2020-10-24 21:32:10 +1100

    refactor useOfferPage to redirect to profile after making offers

--- src/pages/OfferModal/OfferPage/useOfferPage.jsx
+++ src/pages/OfferModal/OfferPage/useOfferPage.jsx
@@ -1,28 +1,30 @@
-import React, { useContext, useEffect } from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useRouteMatch } from 'react-router-dom';
 
 import { makeOffer } from '../../../apis';
-import { LoadTaskContext } from '../../Browse/TaskDetail/LoadTaskContext';
 import OfferPage from './OfferPage';
 
 const OFFER_SUCCESS = 'Offer have been sent to the task owner!';
 const OFFER_FAIL = 'Request sending failed. Please try again later.';
 
 export default function useOfferPage(showMessage) {
-  const loadTaskList = useContext(LoadTaskContext);
-
   const { params: { taskId } } = useRouteMatch();
+  const history = useHistory();
+
+  const [offered, setOffered] = useState(false);
 
   const submitOffer = async () => {
     const result = await makeOffer(taskId);
     const newMessage = result ? OFFER_SUCCESS : OFFER_FAIL;
 
+    if (result) setOffered(true);
+
     return showMessage(newMessage);
   };
 
-  useEffect(() => (
-    () => loadTaskList()
-  ), []);
+  useEffect(() => {
+    if (offered) return history.push('/profile/tasks');
+  }, [offered]);
 
   const page = {};
   page.header = <OfferPage.Header />;

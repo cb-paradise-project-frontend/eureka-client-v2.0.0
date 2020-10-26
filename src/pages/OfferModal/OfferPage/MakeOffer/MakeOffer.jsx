35c86d546305972965e158007bcbbd16cc01e5fc
Ziwei Lan <ziweilan92@gmail.com>
2020-10-27 01:43:22 +1100

    adjust biding price to editable

--- src/pages/OfferModal/OfferPage/MakeOffer/MakeOffer.jsx
+++ src/pages/OfferModal/OfferPage/MakeOffer/MakeOffer.jsx
@@ -1,11 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import styles from './MakeOffer.module.scss';
 
 import { TaskContext } from '../../../Browse/TaskDetail/TaskContext';
+import Input from '../../../../components/Input';
 
 export default function MakeOffer() {
   const { budget } = useContext(TaskContext);
+
+  const [bid, setBid] = useState(budget);
+
+  console.log(Input.Naked);
 
   return (
     <div className={styles.container} >
@@ -14,7 +19,12 @@ export default function MakeOffer() {
       </div>
       <div className={styles.budget}>
         <div className={styles.unit}>$</div>
-        <div className={styles.value}>{budget}</div>
+        <div className={styles.value}>
+          <Input.Naked
+            value={bid}
+            handleChange={setBid}
+          />
+        </div>
       </div>
     </div>
   );

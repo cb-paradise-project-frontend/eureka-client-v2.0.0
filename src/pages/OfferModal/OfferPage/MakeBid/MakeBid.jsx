226e633ee4a1146f1bc93bffdf59ff6d9c26c258
Ziwei Lan <ziweilan92@gmail.com>
2020-10-27 18:26:53 +1100

    rename MakeOffer to MakeBid

--- src/pages/OfferModal/OfferPage/MakeBid/MakeBid.jsx
+++ src/pages/OfferModal/OfferPage/MakeBid/MakeBid.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import styles from './MakeOffer.module.scss';
+import styles from './MakeBid.module.scss';
 
 import Input from '../../../../components/Input';
 
-export default function MakeOffer({ bid, setBid }) {
+export default function MakeBid({ bid, setBid }) {
   return (
     <div className={styles.container} >
       <div className={styles.title} >

f4dfdef3fd4fe5959839d9958b3d35b94d6d6aa0
Ziwei Lan <ziweilan92@gmail.com>
2020-10-30 14:35:20 +1100

    refactor CategoryFilter

--- src/pages/Browse/TaskMenu/CategoryFilter/CategoryFilter.jsx
+++ src/pages/Browse/TaskMenu/CategoryFilter/CategoryFilter.jsx
@@ -1,34 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './CategoryFilter.module.scss';
 
 import useMenuDropDown from '../useMenuDropDown';
 
 const categoryList = {
-  PICKUP: 'Pick up',
-  CLEAN: 'Clean',
-  REMOVAL: 'Removal',
+  pickUp: {
+    value: 'PICKUP',
+    label: 'Pick up',
+  },
+  clean: {
+    value: 'CLEAN',
+    label: 'Clean',
+  },
+  removal: {
+    value: 'REMOVAL',
+    label: 'Removal',
+  },
+  any: {
+    value: '',
+    label: 'Any',
+  },
 };
 
 export default function CategoryFilter({ onSubmit }) {
-  const [MenuDropDown, toggleDropDown] = useMenuDropDown('Category');
+  const [target, setTarget] = useState('any');
+
+  const buttonLabel = `Category - ${categoryList[target].label}`;
+
+  const [MenuDropDown, toggleDropDown] = useMenuDropDown(buttonLabel);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { value: key } = e.target;
+
+    setTarget(key);
+    onSubmit(categoryList[key].value);
+
+    toggleDropDown();
+  };
 
   return (
     <MenuDropDown>
-      <select
-        required
-        className={styles.select}
-        onChange={(e) => onSubmit(e.target.value)}
-      >
-        <option value="" disabled>
-          Select the task category
-        </option>
-        {Object.keys(categoryList).map((key) => (
-            <option key={key} value={key} >
-              {categoryList[key]}
+      <div className={styles.container} >
+        <div className={styles.title} >
+          Select a category
+        </div>
+        <select
+          className={styles.select}
+          value={target}
+          onChange={handleChange}
+          required
+        >
+          {Object.keys(categoryList).map((key) => (
+            <option
+              key={key}
+              value={key}
+            >
+              {categoryList[key].label}
             </option>
-        ))}
-      </select>
+          ))}
+        </select>
+      </div>
     </MenuDropDown>
   );
 }

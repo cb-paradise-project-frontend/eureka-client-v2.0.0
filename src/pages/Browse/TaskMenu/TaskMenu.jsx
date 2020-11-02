60eeec1d2271595dfb05494940ab326eb8fdd423
Ziwei Lan <ziweilan92@gmail.com>
2020-11-03 00:05:19 +1100

    add responsive styles to TaskMenu

--- src/pages/Browse/TaskMenu/TaskMenu.jsx
+++ src/pages/Browse/TaskMenu/TaskMenu.jsx
@@ -1,13 +1,31 @@
 import React, { useState } from 'react';
+import className from 'classnames/bind';
 
 import styles from './TaskMenu.module.scss';
 
 import PriceFilter from './PriceFilter';
 import CategoryFilter from './CategoryFilter';
 import KeywordInput from './KeywordInput';
+import Button from '../../../components/Button';
+
+const cx = className.bind(styles);
 
 export default function TaskMenu({ onFilterChange }) {
   const [activeDropDown, setActive] = useState('');
+
+  const [mobileActive, toggleActive] = useState(false);
+
+  const handleToggle = () => {
+    toggleActive((prevState) => !prevState);
+  };
+
+  const ToggleButton = () => (
+    <div className={styles.toggle_button} >
+      <Button.SearchIcon
+        onClick={handleToggle}
+      />
+    </div>
+  );
 
   const filterToggler = (key) => () => {
     setActive((prevState) => (
@@ -36,17 +54,30 @@ export default function TaskMenu({ onFilterChange }) {
   return (
     <div className={styles.task_menu_wrapper} >
       <div className={styles.task_menu} >
-        <PriceFilter
-          active={activeDropDown === 'price'}
-          onSubmit={handlePriceRangeUpdate}
-          toggler={filterToggler('price')}
-        />
-        <CategoryFilter
-          active={activeDropDown === 'category'}
-          onSubmit={handleFilterChange('category')}
-          toggler={filterToggler('category')}
-        />
-        <KeywordInput onSubmit={handleFilterChange('keyword')} />
+        <div className={cx({
+          left: true,
+          active: !mobileActive,
+        })} >
+          <PriceFilter
+            active={activeDropDown === 'price'}
+            onSubmit={handlePriceRangeUpdate}
+            toggler={filterToggler('price')}
+          />
+          <CategoryFilter
+            active={activeDropDown === 'category'}
+            onSubmit={handleFilterChange('category')}
+            toggler={filterToggler('category')}
+          />
+        </div>
+        <div className={styles.right} >
+          <ToggleButton />
+          <div className={cx({
+            input_wrapper: true,
+            active: mobileActive,
+          })} >
+            <KeywordInput onSubmit={handleFilterChange('keyword')} />
+          </div>
+        </div>
       </div>
     </div>
   );

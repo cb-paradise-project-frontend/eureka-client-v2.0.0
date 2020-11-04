6d05226844195339f663935e82f367fd7965a0b3
Ziwei Lan <ziweilan92@gmail.com>
2020-11-05 00:16:22 +1100

    remove "photo" part from profile

--- src/pages/OfferModal/ProfilePage/form.js
+++ src/pages/OfferModal/ProfilePage/form.js
@@ -1,14 +1,9 @@
-import Photo from './SubPages/Photo';
 import BankAccount from './SubPages/BankAccount';
 import BillingAddress from './SubPages/BillingAddress';
 import Birthday from './SubPages/Birthday';
 import Mobile from './SubPages/Mobile';
 
 const FORM = {
-  photo: {
-    label: 'Profile Picture',
-    Page: Photo,
-  },
   bankAccount: {
     label: 'Bank Account Details',
     Page: BankAccount,

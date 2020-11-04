e045c4924612366c227ee08cae06b05795d20d97
Lucas Guo <gjn1738051666@gmail.com>
2020-11-04 16:02:03 +1000

    navigation change

--- src/pages/Landing/TaskCategories/components/TaskCategoriesItem/TaskCategoriesItem.jsx
+++ src/pages/Landing/TaskCategories/components/TaskCategoriesItem/TaskCategoriesItem.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 import styles from './TaskCategoriesItem.module.scss';
 
-import houseIcon from '../../../../../assets/house.svg';
+// import houseIcon from '../../../../../assets/house.svg';
 
-function TaskCategoriesItem ({
+function TaskCategoriesItem({
   type,
   title,
+  srcUrl,
+  src,
 }) {
   return (
     <div className={styles.taskCategoriesItem}>
       <button className={styles.taskCategoriesItem__button}>
         <div>
-          <img src={houseIcon} alt={type}/>
+          {/* <img src={src} alt={type}/> */}
+          {/* {srcUrl} */}
+          {/* <i class={"fas fa-broom"}></i> */}
         </div>
         <p>{title}</p>
       </button>

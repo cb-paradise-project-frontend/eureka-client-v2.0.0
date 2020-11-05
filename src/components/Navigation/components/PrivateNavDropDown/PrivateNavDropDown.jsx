84a8d139cd8d11cae9f710bb0878a0d47fb8aecd
Ziwei Lan <ziweilan92@gmail.com>
2020-11-05 23:38:54 +1100

    refactor Private and PrivateNavDropDown for new modal controlling logics

--- src/components/Navigation/components/PrivateNavDropDown/PrivateNavDropDown.jsx
+++ src/components/Navigation/components/PrivateNavDropDown/PrivateNavDropDown.jsx
@@ -1,30 +1,36 @@
-import React  from 'react';
+import React, { useEffect }  from 'react';
 import { Link } from 'react-router-dom';
 import styles from './PrivateNavDropDown.module.scss';
 
-import ToggleContent from '../../../ToggleContent';
 import Button from '../../../Button';
 import PostTask from '../../../../pages/PostTask';
+import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
 // import LogOut from '../LogOut';
 
-function PrivateNavDropDown() {
+function PrivateNavDropDown({
+  togglePostTask,
+  pageToggler,
+  showDropDown,
+}) {
+  const { pathname } = useLocation();
+
   const PostTaskButton = () => (
-    <ToggleContent
-      toggle={(toggler) => (
-        <div className={styles.privateNavDropDownItem} >
-          <Button.Text
-            color={'navMobile'}
-            onClick={toggler}
-          >
-            Post a Task
-          </Button.Text>
-        </div>
-      )}
-      content={(toggler) => (
-        <PostTask pageToggler={toggler} />
-      )}
-    />
+    <div className={styles.privateNavDropDownItem} >
+      <Button.Text
+        color={'navMobile'}
+        onClick={() => {
+          togglePostTask();
+          pageToggler();
+        }}
+      >
+        Post a Task
+      </Button.Text>
+    </div>
   );
+
+  useEffect(() => {
+    if (showDropDown) pageToggler();
+  }, [pathname]);
 
   return (
     <div className={styles.private}>

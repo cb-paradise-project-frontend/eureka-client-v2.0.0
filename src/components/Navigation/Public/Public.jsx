cc941ee60e0007db95fb630b49a0c656b27f6285
Lucas Guo <gjn1738051666@gmail.com>
2020-11-05 13:33:49 +1000

    nav bar public and private state upgrade

--- src/components/Navigation/Public/Public.jsx
+++ src/components/Navigation/Public/Public.jsx
@@ -1,22 +1,125 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Public.module.scss';
 
-import NavigationMobile from '../components/NavigationMobile';
-import NavigationWeb from '../components/NavigationWeb';
+import logo from '../../../assets/logo.svg';
+import postTaskLogo from '../../../assets/postTaskLogo.svg';
 
-function Public() {
-  return (
-    <React.Fragment>
-      <div className={styles.menuMobile}>
-        <NavigationMobile />
-      </div>
+import Button from '../../Button';
+import ToggleContent from '../../ToggleContent';
+import PostTask from '../../../pages/PostTask';
+import DropDown from '../components/DropDown';
 
-      <div className={styles.web}>
-        <NavigationWeb />
+// import NavigationMobile from '../components/NavigationMobile';
+// import NavigationWeb from '../components/NavigationWeb';
+
+class Public extends Component {
+  constructor() {
+    super();
+
+    this.state = { clicked:false }
+  }
+
+  handleClick = () => {
+    this.setState((prevState) => ({ clicked: !prevState.clicked}));
+  }
+
+  render() {
+    const PostTaskButton = () => (
+      <ToggleContent
+        toggle={(toggler) => (
+          <div className={styles.post_task_button_wrapper} >
+            <Button
+              color={'navy'}
+              onClick={toggler}
+              size={'navbar'}
+            >
+              Post a Task
+            </Button>
+          </div>
+        )}
+        content={(toggler) => (
+          <PostTask pageToggler={toggler} />
+        )}
+      />
+    );
+
+    const PostTaskIconButton = () => (
+      <ToggleContent
+        toggle={(toggler) => (
+          <div className={styles.post_task_icon_button_wrapper}>
+            <Button.IconButton
+              onClick={toggler}
+            >
+              <img src={postTaskLogo} alt={postTaskLogo} />
+            </Button.IconButton>
+          </div>
+        )}
+        content={(toggler) => (
+          <PostTask pageToggler={toggler} />
+        )}
+      />
+    );
+
+    const PostTaskButtonMobile = () => (
+      <ToggleContent
+        toggle={(toggler) => (
+          <div className={styles.post_task_button_wrapper} >
+            <Button
+              color={'mint'}
+              onClick={toggler}
+              size={'small'}
+            >
+              Post a Task
+            </Button>
+          </div>
+        )}
+        content={(toggler) => (
+          <PostTask pageToggler={toggler} />
+        )}
+      />
+    );
+
+    return (
+      <div className={styles.Public}>
+        <div
+          className={styles.responsiveButton}
+          onClick={this.handleClick}
+        >
+          <i className={this.state.clicked ? 'far fa-times-circle' : 'fas fa-bars'}></i>
+        </div>
+
+        <Link
+          className={styles.logo}
+          to="/"
+        >
+          <img src={logo} alt={logo} />
+          <p>Eureka</p>
+        </Link>
+
+        <div className={styles.left} >
+          <PostTaskButton />
+          <Link
+            className={styles.browseTasks}
+            to="/tasks"
+          >
+            Browse tasks
+          </Link>
+        </div>
+
+        <div className={styles.responsivePostTask}>
+          <PostTaskIconButton />
+          <div className={this.state.clicked ? styles.postTaskButtonActive : styles.postTaskButton}>
+            <PostTaskButtonMobile />
+          </div>
+        </div>
+
+        <div  className={this.state.clicked ? styles.mobileNavDropDownActive : styles.mobileNavDropDown}>
+          <DropDown />
+        </div>
       </div>
-    </React.Fragment>
-  );
+    );
+  }
 }
 
 export default Public;

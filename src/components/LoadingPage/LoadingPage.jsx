20bf5c19830ab8e99c5496cd77d8f4fcfed8cb62
Ziwei Lan <ziweilan92@gmail.com>
2020-11-11 16:54:35 +1100

    add layout style to LoadingPage

--- src/components/LoadingPage/LoadingPage.jsx
+++ src/components/LoadingPage/LoadingPage.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Overlay from '../Overlay';
 import Spinner from '../Spinner';
 
+const Layout = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 export default function LoadingPage() {
   return (
     <Overlay>
-      <Spinner />
+      <Layout>
+        <Spinner />
+      </Layout>
     </Overlay>
   );
 }
